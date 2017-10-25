860e8d6b5e31d6cd690c6f80e295e01b0751f4dc
Vladimir Tambovtsev <tambovcev99@mail.ru>
2017-10-25 22:41:43 +0300

    Added sockets

--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socket = require('socket.io');
 
 var publicPath = path.join(__dirname, '../public') // just go to public directory
 const port = process.env.PORT || 3000; // Heroku or Localhost deployemnt
 
 var app = express();
+var server = http.createServer(app); // create server for sockets
+var io = socket(server); // add socket
 
 app.use(express.static(publicPath)); // add index static files 
 
+io.on('connection', (socket) => {
+	console.log("new user connected");
+
+	socket.on('disconnect', () => {
+		console.log("user was disconnected");
+	});
+
+
+});
  
-app.listen(port, () => {
+server.listen(port, () => {
 	console.log(`Server is up on ${port}`);
 });
