9241e57e3d65149b7d6b74e2102a77ec0de4bd05
Vladimir Tambovtsev <tambovcev99@mail.ru>
2017-10-26 00:10:36 +0300

    Added message utils

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const http = require('http');
 const express = require('express');
 const socket = require('socket.io');
 
+
+const {generateMessage} = require('./utils/message');
 var publicPath = path.join(__dirname, '../public') // just go to public directory
 const port = process.env.PORT || 3000; // Heroku or Localhost deployemnt
 
@@ -15,25 +17,14 @@ app.use(express.static(publicPath)); // add index static files
 io.on('connection', (socket) => {
 	console.log("new user connected");
 
-	socket.emit('newMessage', {
-		from: "Admin",
-		text: "Welcome to the chat app"
-	})
+	socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat App'));
 
-	// get event all except one
-	socket.broadcast.emit('newMessage', {
-		from: "Admin",
-		text: "New user joined",
-		createdAt: new Date().getTime()
-	})
+	socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
 
 	socket.on('createMessage', (message) => {
 		console.log('Message:message', message);
-		io.emit('newMessage', {
-			from: message.from,
-			text: message.text,
-			createdAt: new Date().getTime()
-		})
+		io.emit('newMessage', generateMessage(message.from, message.text));
 		/*
 		socket.broadcast.emit('newMessage', {
 			from: message.from,
